313b80c86178de108826ca0d0494d0c2412fe302
Marina <marina.gonzalez2024@hilasal.superate.org.sv>
2024-10-15 09:00:50 -0600

    Conexion

---
+++ src/Carpeta de resultados/Sociales/tratadodv.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client';
+import  TratadoV from './TratadoV';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TratadoV />
+  </React.StrictMode>
+);
